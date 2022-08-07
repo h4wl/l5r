94bd7a6166f63517a997fc9c9095b5067801c21d
David Hawley Brim <davidbrim@outlook.com>
2022-08-07 09:02:34 -0500

    Add deploy to GitHub Pages task in bootstrapper

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 ﻿await Bootstrapper
   .Factory
   .CreateWeb(args)
+  .DeployToGitHubPagesBranch(
+        "h4wl",
+        "l5r",
+        Config.FromSetting<string>("GITHUB_TOKEN"),
+        "gh-pages"
+    )
   .RunAsync();
